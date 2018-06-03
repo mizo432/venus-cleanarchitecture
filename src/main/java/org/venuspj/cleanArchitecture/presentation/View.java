3b57b7ba1b0d2b428054e9bcf4b5ba53349ee667
mizoguchikenichi <hachirouuemon.mizoguchi@gmail.com>
2018-06-04 08:17:38 +0900

    add javadoc comment

--- src/main/java/org/venuspj/cleanArchitecture/presentation/View.java
+++ src/main/java/org/venuspj/cleanArchitecture/presentation/View.java
@@ -1,5 +1,9 @@
 package org.venuspj.cleanArchitecture.presentation;
 
+/**
+ * View.
+ * @param <D> ViewModelの型
+ */
 public interface View<D> {
-    View<D> setViewModel(D aViewModel);
+    View<D> setViewModel(D viewModel);
 }
--- src/main/java/org/venuspj/cleanArchitecture/useCase/UseCaseInteractor.java
+++ src/main/java/org/venuspj/cleanArchitecture/useCase/UseCaseInteractor.java
@@ -1,10 +1,20 @@
 package org.venuspj.cleanArchitecture.useCase;
 
+
 /**
  * アプリケーションレベルの機能(ユースケース)のインターフェイス.
+ *
+ * @param <I> ユースケースのインプットポート
+ * @param <O> ユースケースのアウトプットポート
  */
 public interface UseCaseInteractor<I extends UseCaseInputPort, O extends UseCaseOutputPort> {
 
+    /**
+     * ユースケースの開始をします。
+     *
+     * @param request  リクエスト用インプットポート
+     * @param response レスポンス用アウトプットポート
+     */
     void start(I request, O response);
 
 }
